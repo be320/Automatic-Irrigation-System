344e140308286923627e62561b14f3e1cca14e5c
Ahmed Bahaa <33236153+be320@users.noreply.github.com>
2022-10-11 23:23:35 +0200

    Initial Commit

---
+++ src/main/java/com/farm/irregation/IrregationApplication.java
@@ -0,0 +1,13 @@
+package com.farm.irregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrregationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrregationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farm/irregation/IrregationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farm.irregation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrregationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
